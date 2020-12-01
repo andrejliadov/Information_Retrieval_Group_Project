6155797024a6580c71fe2ee65be8fdd2c48c33cd
pgilsena <pgilsena@tcd.ie>
2020-12-01 20:30:47 +0000

    Cleaned up LA Times parser (more efficient)

--- maven-group/src/main/java/com/assignment2/parser/LATParser.java
+++ maven-group/src/main/java/com/assignment2/parser/LATParser.java
@@ -13,6 +13,7 @@ import org.apache.lucene.document.TextField;
 
 import org.jsoup.Jsoup;
 // import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 
@@ -31,27 +32,18 @@ public class LATParser
         if (datasetFiles != null) {
             for (File file : datasetFiles) {
                 try {
-                    // Loop through lines in file
-                    BufferedReader bFile = new BufferedReader(new FileReader(file));
-                    String fileLine = "";
-                    String article = "";
+                    if(file.getName().contains(".txt")) {
+                        // Ignore readchg.txt and readmela.txt
+                        continue;
+                    }
 
-                    while ((fileLine = bFile.readLine()) != null) {
+                    org.jsoup.nodes.Document jsoupDoc = Jsoup.parse(file, "UTF-8");
 
-                        if (fileLine.startsWith("<DOC>")) {
-                            // If line contains <DOC> start new doc
-                        } else if (fileLine.startsWith("</DOC>")) {
-                            // If line contains </DOC>
-                            // create Lucene document for article
-                            Document doc = getElements(article);
-                            documents.add(doc);
+                    Elements docTags = jsoupDoc.getElementsByTag("DOC");
 
-                            // Reset article to empty string for next doc
-                            article = "";
-                        } else {
-                            // Otherwise add line to string
-                            article = article.concat(fileLine);
-                        }
+                    for (Element docTag : docTags) {
+                        Document doc = getElements(docTag);
+                        documents.add(doc);
                     }
                 } catch (Exception e) {
                     System.out.println(e);
@@ -62,22 +54,20 @@ public class LATParser
         return documents;
     }
 
-    private static Document getElements(String article) {
+    private static Document getElements(org.jsoup.nodes.Element doc) {
         // Did not parse list: <LENGTH> <GRAPHIC>
-        // Document doc = Jsoup.parse(article);
-        org.jsoup.nodes.Document doc = Jsoup.parse(article);
 
         // These fields appear in every document
         String docNumber = doc.select("DOCNO").first().ownText();
-        String docId = doc.select("DOCID").first().ownText();
+        // String docId = doc.select("DOCID").first().ownText();
 
         // These fields do not appear in every document
         // All have inner <p> tag (e.g. <TYPE><P>Wire</P></TYPE>)
         String headLine = getValue(doc, "HEADLINE");
-        String byLine = getValue(doc, "BYLINE");
-        String type = getValue(doc, "TYPE");
-        String date = getValue(doc, "DATE");
-        String section = getValue(doc, "SECTION");
+        // String byLine = getValue(doc, "BYLINE");
+        // String type = getValue(doc, "TYPE");
+        // String date = getValue(doc, "DATE");
+        // String section = getValue(doc, "SECTION");
 
         // Body of article has multiple <p> tags
         String contents = getArticleBody(doc, "TEXT");
@@ -91,7 +81,7 @@ public class LATParser
         return document;
     }
 
-    private static String getValue(org.jsoup.nodes.Document doc, String tag) {
+    private static String getValue(Element doc, String tag) {
         Elements element = doc.select(tag);
 
         if (element.isEmpty()) {
@@ -105,7 +95,7 @@ public class LATParser
         return value;
     }
 
-    private static String getArticleBody(org.jsoup.nodes.Document doc, String tag) {
+    private static String getArticleBody(Element doc, String tag) {
         // Get article contents - made up of multiple <P> tags
         // Concat tag content without tags
         String value = doc.select(tag + " P").text();
