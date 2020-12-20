da72493d76e8b090b23774922f205fc4e6ec4691
Andrej <andrej.liadov@gmail.com>
2020-12-20 15:34:21 +0000

    Lemmatisation, n-grams, synonym query expansion added 	modified:   pom.xml 	modified:   qresults 	modified:   src/main/java/com/assignment2/Main.java 	modified:   src/main/java/com/assignment2/MyAnalyzer.java

--- maven-group/src/main/java/com/assignment2/Main.java
+++ maven-group/src/main/java/com/assignment2/Main.java
@@ -7,14 +7,17 @@ import com.assignment2.search.Search;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.store.Directory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +27,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Analyzer analyzer = new MyAnalyzer();
         Similarity similarityFunction = new BM25Similarity();
+        
+        String test = "Still, Mr. Barr’s resignation allows him to avoid further confrontation with the president over his refusal to advance Mr. Trump’s attempts to rewrite the election results or his efforts to interfere in criminal inquiries into Mr. Biden’s family.";
+        TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(test));
+        
+        CharTermAttribute cattr = tokenStream.addAttribute(CharTermAttribute.class);
+        tokenStream.reset();
+        while (tokenStream.incrementToken()) {
+          System.out.println(cattr.toString());
+        }
+        tokenStream.end();
+        tokenStream.close();
+
         
         Directory directory = BuildIndex.startBuildIndex(analyzer);
         List<NumberedQuery> queries = QueryReader.readQueries(analyzer);
--- maven-group/src/main/java/com/assignment2/MyAnalyzer.java
+++ maven-group/src/main/java/com/assignment2/MyAnalyzer.java
@@ -6,27 +6,43 @@ import org.apache.lucene.analysis.en.*;
 import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
 import org.apache.lucene.analysis.miscellaneous.TrimFilter;
 import org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilter;
+import org.apache.lucene.analysis.shingle.ShingleFilter;
 import org.apache.lucene.analysis.snowball.SnowballFilter;
 import org.apache.lucene.analysis.standard.ClassicFilter;
 import org.apache.lucene.analysis.standard.ClassicTokenizer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.synonym.SynonymFilter;
 import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
 import org.apache.lucene.analysis.synonym.SynonymMap;
+import org.apache.lucene.analysis.synonym.WordnetSynonymParser;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.util.ElisionFilter;
 import org.apache.lucene.util.CharsRef;
 import org.tartarus.snowball.ext.EnglishStemmer;
 
+import com.assignment2.filter.LemmaFilter;
+
+import edu.stanford.nlp.simple.Sentence;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.file.Paths;
+import java.text.ParseException;
 
 public class MyAnalyzer extends StopwordAnalyzerBase {
 
     private static final String stop_file_path = "stopwords/stopwords2.txt";
+    private static final String synonym_file_path = "wn_s.pl";
+    private static Reader synonymReader;
+    private static String temp = "";
 
     public MyAnalyzer() throws IOException {
         super(StopwordAnalyzerBase.loadStopwordSet(Paths.get(stop_file_path)));
+        synonymReader = new FileReader(synonym_file_path);
+        
     }
 
     protected TokenStreamComponents createComponents(String fieldName) {
@@ -38,9 +54,24 @@ public class MyAnalyzer extends StopwordAnalyzerBase {
         TokenStream tokenStream = new EnglishPossessiveFilter(stdTokenizer);
         tokenStream = new LowerCaseFilter(tokenStream);
         tokenStream = new StopFilter(tokenStream, stopwords);
+        tokenStream = new LemmaFilter(tokenStream);
+        /*WordnetSynonymParser parser = new WordnetSynonymParser(true, false, new StandardAnalyzer());
+        
+		try {
+			//parser.parse(synonymReader);
+		} catch (IOException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			//tokenStream = new SynonymGraphFilter(tokenStream, parser.build(), true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 //        tokenStream = new FlattenGraphFilter(
 //                new SynonymGraphFilter(tokenStream, "",
-//                        true));
+//                        true));*/
 //
 //
 
@@ -50,11 +81,11 @@ public class MyAnalyzer extends StopwordAnalyzerBase {
 //                | WordDelimiterGraphFilter.SPLIT_ON_NUMERICS
 //                | WordDelimiterGraphFilter.STEM_ENGLISH_POSSESSIVE, null);
 
-        tokenStream = new SnowballFilter(tokenStream,
-                new EnglishStemmer());
+        //tokenStream = new SnowballFilter(tokenStream,
+          //      new EnglishStemmer());
 
-        tokenStream = new EnglishMinimalStemFilter(tokenStream);
-        tokenStream = new PorterStemFilter(tokenStream);
+        //tokenStream = new EnglishMinimalStemFilter(tokenStream);
+        //tokenStream = new PorterStemFilter(tokenStream);
 
         // ElisionFilter
 
